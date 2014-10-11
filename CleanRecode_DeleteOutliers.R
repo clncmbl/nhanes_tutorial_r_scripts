1a9ec63de3209b3ebc963d9a5862ed7fb04f919e
Colin Campbell <cln.cmbl@gmail.com>
2014-10-11 08:33:55 -0500

    Made the outlier deletion script source and output much more readable by using dplyr's summarise.

--- CleanRecode_DeleteOutliers.R
+++ CleanRecode_DeleteOutliers.R
@@ -1,17 +1,9 @@
 # Step 3 of task 3a
 
-wtdmean <- function (x) {
-  wtd.mean(x$LBXTC, weights=x$WTMEC4YR, na.rm=T)
-}
-
-wtdstderr <- function (x) {
-  n <- sum(!is.na(x$LBXTC))
-  sqrt(wtd.var(x$LBXTC, weights=x$WTMEC4YR, na.rm=T)/n)
-}
+library(dplyr)
+library(Hmisc)
 
 demo_bp <- readRDS("../data/demo_bp2b.rds")
-
-###demo_bp[!is.na(demo_bp$LBXTC) & demo_bp$LBXTC > 600,]
 
 demo_bp$race <- factor(demo_bp$RIDRETH1,
                       labels=c("Mexican American",
@@ -22,21 +14,15 @@ demo_bp$race <- factor(demo_bp$RIDRETH1,
 
 demo_bp <- demo_bp[demo_bp$RIDSTATR==2 & demo_bp$RIDAGEYR>=20,]
 
-table(demo_bp$race)
-
-lapply(split(demo_bp, demo_bp$race), wtdmean)
-
-lapply(split(demo_bp, demo_bp$race), wtdstderr)
+summarise(group_by(demo_bp,race),
+          n=length(race),
+          wtd_mean=wtd.mean(LBXTC, weights=WTMEC4YR),
+          wtd_se=sqrt(wtd.var(LBXTC, weights=WTMEC4YR)/sum(!is.na(LBXTC))))
 
 exclu3 <- demo_bp[is.na(demo_bp$LBXTC) | demo_bp$LBXTC < 600,]
 
-table(exclu3$race)
-#exclu3 <- exclu3[exclu3$RIDSTATR==2 & exclu3$RIDAGEYR>=20 & !is.na(exclu3$LBXTC),]
-
-#lapply(split(exclu3, exclu3$race), function(x) wtd.mean(x$LBXTC, weights=x$WTMEC4YR))
-lapply(split(exclu3, exclu3$race), wtdmean)
-lapply(split(exclu3, exclu3$race), wtdstderr)
-
-#demo_bp <- demo_bp[demo_bp$RIDSTATR==2 & demo_bp$RIDAGEYR>=20,]
-
-# TODO: See wtd.stats in Hmisc
+summarise(group_by(exclu3,race),
+          n=length(race),
+          wtd_mean=wtd.mean(LBXTC, weights=WTMEC4YR),
+          wtd_se=sqrt(wtd.var(LBXTC, weights=WTMEC4YR)/sum(!is.na(LBXTC))))
+          
